2305a7908e653904f58ee9593e8e3802d8266e28
Pieter Reyneke <pieter@asit4u.co.za>
2020-05-24 13:17:22 +0200

    added spring project

---
+++ cfd-api-scheduler/src/main/java/com/gt247/cfdapischeduler/CfdApiSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.gt247.cfdapischeduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfdApiSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfdApiSchedulerApplication.class, args);
+	}
+
+}
